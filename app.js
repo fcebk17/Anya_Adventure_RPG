f0e7c2bf828aba78312e8fe9baa7d958d674bde2
Jun3631 <72019035+Jun3631@users.noreply.github.com>
2022-12-05 18:11:03 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -29,6 +29,9 @@ app.get('/recapVideo', function (req, res) {
 app.get('/anyaIntro', function (req, res){
     res.render('anyaIntro', {'title': 'Anya大冒險RPG - 自我介紹'});
 });
+app.get('/level1', function (req, res){
+    res.render('level1', {'title': 'Anya大冒險RPG - 關卡一'});
+});
 
 app.listen(3000); // 設定 port 3000
 // 要用 axios.get
