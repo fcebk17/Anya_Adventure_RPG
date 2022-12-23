3391676bb15e38b580b3bf964bb5f6dced41c6a8
fcebk17 <fcebk17@gmail.com>
2022-12-23 15:20:44 +0800

    fix: yorIntro page
    + change button form
    + redirect true path

--- public/js/yorIntro.js
+++ public/js/yorIntro.js
@@ -2,8 +2,9 @@ var body = document.querySelector("body");
 var counter = 0;
 var counterButton = 0;
 var choose_button_string = ["button1","button2","button3","button4"];
-var choose_button_value = ["找安妮亞在哪","聽音樂放鬆","遛狗","給女兒獎勵"];
+var choose_button_value = ["找安妮亞在哪","遛狗","聽音樂放鬆","給女兒獎勵"];
 var text_string = ["我是伯林特市政府的女性公務員約兒•佛傑，其實真正的職業是殺手，代號〈睡美人〉，但也是安妮亞的媽媽，話說安妮亞去哪兒了!?不過剛工作完確實有點累...該做什麼呢?"];
+var href = ["/anyaOS", "/WalkTheDog", "/enjoyMusic", "/yorAward"];
 
 $('body').css('background-image','url(/img/youIntro.png)');
 function spaceEvent(e) {
@@ -42,8 +43,9 @@ function content(){
 //new button
 //按鈕
 function button(choose_button){
-    var MyDiv = document.getElementById(choose_button_string[choose_button]);
-    MyDiv.textContent = choose_button_value[choose_button];
+    var display = document.getElementById("display");
+
+    display.innerHTML += "<a href = '"+ href[counterButton] +"'><button id = 'button" + parseInt(counterButton+1) + "' class = 'button" + parseInt(counterButton+1) +"'>" +"<span class='ansbt'>"+ choose_button_value[choose_button]+"</span></button></a>";
     counterButton++;
 }
 body.addEventListener('keydown', spaceEvent, false);
