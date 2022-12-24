3332602f48114c581b96fb023755faf7993a87b1
Jun3631 <72019035+Jun3631@users.noreply.github.com>
2022-12-25 02:21:44 +0800

    finalPage
    finalPage

---
+++ public/js/finalPage.js
@@ -0,0 +1,12 @@
+
+setTimeout(win,7000);
+function win(){
+    text = document.getElementById("text");
+    text.innerHTML = "<h1 class = 'animate__animated animate__fadeInDownBig'>     成為星光安妮亞 </h1>"
+    setTimeout(vedio,7500);
+}
+function vedio(){
+
+    window.location.href = "/finalvideo";
+}
+
